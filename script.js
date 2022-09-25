a96b4b0be4a9cebbf8cdd7b09581ba2968c32f69
nhartm <natalie.hartmann98@gmail.com>
2022-09-24 19:02:53 -0700

    added variables to discribe textblocks.

--- script.js
+++ script.js
@@ -1,9 +1,36 @@
 var timeDisplayEl = $('#time-display');
-var userInputEl = $('input[name=9:00]');
-var formEl = $('event-form');
-//var rightNow = moment();
-//$('time-display').text(rightNow)
+var formEl = $('#event-form');
+var rightNow = moment();
+$('time-display').text(rightNow);
+var saveButton = document.getElementById("save");
+var hourNineInput = $('input[name="nine"]');
+var hourTenInput = $('input[name="ten"]');
+var hourElevenInput = $('input[name="eleven"]');
+var hourTwelveInput = $('input[name="twelve"]');
+var hourOneInput = $('input[name="one"]');
+var hourTwoInput = $('input[name="two"]');
+var hourThreeInput = $('input[name="three"]');
+var hourFourInput = $('input[name="four"]');
+var hourFiveInput = $('input[name="five"]');
 
+saveButton.addEventListener("click", function(event) {
+   event.preventDefault();
+    var formInputEl = {
+    hourNine: hourNineInput.value.trim(),
+    hourTen: hourTenInput.val(),
+    hourEleven: hourElevenInput.value.trim(),
+    hourTwelve: hourTwelveInput.value.trim(),
+    hourOne: hourOneInput.value.trim(),
+    hourTwo: hourTwoInput.value.trim(),
+    hourThree: hourThreeInput.value.trim(),
+    hourFour: hourFourInput.value.trim(),
+    hourFive: hourFiveInput.value.trim(),
+
+     
+    }
+    localStorage.setItem("formInputEl",JSON.stringify(formInputEl));
+   
+    });
 
 // Adds date to the top of the page
 function displayTime() {
@@ -11,13 +38,11 @@ function displayTime() {
     timeDisplayEl.text(rightNow);
 }
 
-function handleScheduleData (event){
-event.preventDefault();
 
-console.log("9:00", userInputEl.val())
-}
 
 function addColor (event){
+    $('#event-form').children('ul').children().addClass('input-group input-group-lg').css;
+
     if (rightNow() === isFuture) {
        element.classList.add('.future')
     } else if (rightNow() === isPast) {
@@ -32,7 +57,7 @@ var isFuture = rightNow > rightNow
 
 var isPast = rightNow < rightNow
 
-formEl.on('submit', handleScheduleData)
+//handleScheduleData.on('submit', saveButton)
+//timeDisplayEl()
 
-
-
+setInterval(displayTime, 1000);
